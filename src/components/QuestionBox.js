4e75b1bdb01117b418469f93dd35e315befd831c
Harshith Renikindi <159125359+hrenikindi@users.noreply.github.com>
2024-05-23 11:31:01 +0000

    Created Quiz Game using React for CA-4

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
-export default function QuestionBox() {
+function QuestionBox({
+  question,
+  currentQuestionIndex,
+  totalQuestions,
+  onAnswer,
+  highlight,
+  toggleHighlight,
+}) {
+  const handleHighlight = (value) => {
+    toggleHighlight(value);
+  };
+  const optionsButtons = [];
+  for (let i = 0; i < question.options.length; i++) {
+    const option = question.options[i];
+    optionsButtons.push(
+      <button key={option.id} onClick={() => onAnswer(option.isCorrect)}>
+        {option.text}
+      </button>,
+    );
+  }
+
   return (
     <div>
-      
+      <h2>
+        Question: {currentQuestionIndex + 1} out of {totalQuestions}
+      </h2>
+      <h3 className={`question-text ${highlight ? "highlighted" : ""}`}>
+        {question.text}
+      </h3>
+      <div className="question-options">{optionsButtons}</div>
+      <div>
+        <button
+          className="highlight-button"
+          onClick={() => handleHighlight(true)}
+        >
+          Highlight
+        </button>
+        <button
+          className="remove-highlight-button"
+          onClick={() => handleHighlight(false)}
+        >
+          Remove Highlight
+        </button>
+      </div>
     </div>
-  )
+  );
 }
+
+export default QuestionBox;
